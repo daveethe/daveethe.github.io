65d29d99f515deb68af1d831bb9a2f4cf75063a3
daveethe <111148401+daveethe@users.noreply.github.com>
2024-10-01 22:10:51 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const loginForm = document.getElementById('loginForm');
     const errorMessage = document.getElementById('errorMessage');
+    const togglePassword = document.getElementById('togglePassword');
+    const passwordField = document.getElementById('password');
 
     loginForm.addEventListener('submit', async function(event) {
         event.preventDefault();
 
         // Prendiamo la password inserita dall'utente
-        const password = document.getElementById('password').value;
+        const password = passwordField.value;
 
         // Invia la password al server per la verifica
         try {
@@ -31,5 +33,16 @@ document.addEventListener('DOMContentLoaded', function() {
         } catch (error) {
             console.error('Errore durante il login:', error);
         }
+    });
+
+    // Aggiungi la funzionalità per mostrare/nascondere la password
+    togglePassword.addEventListener('click', function() {
+        // Cambia il tipo di input da "password" a "text" o viceversa
+        const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordField.setAttribute('type', type);
+
+        // Cambia l'icona
+        this.classList.toggle('fa-eye');
+        this.classList.toggle('fa-eye-slash');
     });
 });
