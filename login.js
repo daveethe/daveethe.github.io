6b9ed17fb996cf09a2767b57f7c90e3a3485a4ac
daveethe <111148401+daveethe@users.noreply.github.com>
2024-10-01 21:11:48 +0200

    Update login.js

--- login.js
+++ login.js
@@ -2,23 +2,34 @@ document.addEventListener('DOMContentLoaded', function() {
     const loginForm = document.getElementById('loginForm');
     const errorMessage = document.getElementById('errorMessage');
 
-    loginForm.addEventListener('submit', function(event) {
+    loginForm.addEventListener('submit', async function(event) {
         event.preventDefault();
 
-        // Password prestabilita
-        const presetPassword = 'Foufou12$'; // Sostituisci con la tua password
-
-        // Valore inserito dall'utente
+        // Prendiamo la password inserita dall'utente
         const password = document.getElementById('password').value;
 
-        // Verifica della password
-        if (password === presetPassword) {
-            // Password corretta, salva lo stato di autenticazione e reindirizza
-            sessionStorage.setItem('authenticated', 'true');
-            window.location.href = 'index.html'; // Reindirizza alla tua pagina principale
-        } else {
-            // Password errata, mostra il messaggio di errore
-            errorMessage.style.display = 'block';
+        // Invia la password al server per la verifica
+        try {
+            const response = await fetch('https://vacation-planner-backend.onrender.com/api/vacations/verifyPassword', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ password }),
+            });
+
+            const result = await response.json();
+
+            if (result.success) {
+                // Se la password è corretta, reindirizza alla home
+                sessionStorage.setItem('authenticated', 'true');
+                window.location.href = 'index.html';
+            } else {
+                // Se la password è errata, mostra un messaggio di errore
+                errorMessage.style.display = 'block';
+            }
+        } catch (error) {
+            console.error('Errore durante il login:', error);
         }
     });
 });
